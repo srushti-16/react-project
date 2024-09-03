c8b14c54f56f7c4babbe2d7bdd49953f2313564f
srushti-16 <72909661+srushti-16@users.noreply.github.com>
2024-09-03 15:58:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/react-project/',  // Replace 'your-repository-name' with the actual name of your repo
+});
+
